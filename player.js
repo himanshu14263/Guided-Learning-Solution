c4a04662b1e01c2f4b9f7b495bb0501be1da16f7
himanshu14263 <lelouch0lampourge@gmail.com>
2021-02-08 19:24:48 +0530

    basic sript to run using TamperMonkey

---
+++ player.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         GLS
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.google.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+})();
